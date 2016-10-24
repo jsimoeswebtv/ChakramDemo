c1fb2ca54c4f2433630ef163262a2c76b9b40974
Joao Simoes <jsimoes@outlook.com>
2016-10-24 16:45:58 +0200

    initial commit

---
+++ ChakramDemo/server.js
@@ -0,0 +1,14 @@
+﻿var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes.js");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+
+var server = app.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
